2de0798cc6d859e73e8a1642d3617013c1c3a0d1
Dustin <dustin.anthony.coffman@gmail.com>
2019-12-20 12:58:13 -0600

    search feature

--- main_app/views.py
+++ main_app/views.py
@@ -8,21 +8,22 @@ import os
 
 # Create your views here.
 def show(request):
-    print('the route is working!!!')
-    # myKey = os.environ['SECRET_KEY']
-    # appKey = os.environ['APP_ID']
-    # req = requests.get(f"http://rest.bandsintown.com/artists/metallica?app_id={appKey}")
-    # print('----------------<(^_^)>-----------------------')    
-    # req = req.json()
-    # print (req['name'])
-    # print('----------------<(^_^)>-----------------------')
-    # return render(request, 'index.html')
-    # make request to eventul api
-
-def home(request):
+    artist = request.POST['name_field']
     myKey = os.environ['SECRET_KEY']
     appKey = os.environ['APP_ID']
-    req = requests.get(f"http://eventful.com/json/events?q=music&l=TX&t=December+2019")
+    req = requests.get(f"http://rest.bandsintown.com/artists/{artist}?app_id={appKey}")
+    print('----------------<(^_^)>-----------------------')    
+    req = req.json()
+    print (req['name'])
+    print('----------------<(^_^)>-----------------------')
+    return render(request, 'artists/index.html')
+
+
+def home(request):
+    print(request)
+    myKey = os.environ['SECRET_KEY']
+    appKey = os.environ['APP_ID']
+    req = requests.get(f"http://eventful.com/json/events?q=music&l=TX&t=December+2019app_id=gssq2hLc4npkxj69")
     print(req)
     print('----------------<(^_^)>-----------------------')    
     req = req.json()
